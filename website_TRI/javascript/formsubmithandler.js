f300c997ba9ba6ef835b7ef4bf66840151d52392
NasywaAyu21 <165874720+NasywaAyu21@users.noreply.github.com>
2025-07-12 03:49:09 +0700

    Delete website_TRI directory

--- website_TRI/javascript/formsubmithandler.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
-  if (!form) return;
-
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    showThankYouPopup(); // Fungsi dari popup-thankyou.js
-  });
-});
